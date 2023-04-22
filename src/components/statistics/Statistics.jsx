d39b84bb90ad2e326bdf58a66819192b2b63f486
OlegArt1 <113111922+OlegArt1@users.noreply.github.com>
2023-04-23 00:15:00 +0300

    Update

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,115 +1,39 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { Notification } from "./Notification";
+/* import PropTypes from "prop-types"; */
 import Css from "./Statistics.module.css";
 
-export class Statistics extends Component
+function getRandomHexColor()
 {
-    state =
-    {
-        good: 0,
-        neutral: 0,
-        bad: 0,
-        total: 0,
-        positivePercentage: 0
-    };
-    constructor()
-    {
-        super();
-        this.handleGood = this.handleGood.bind(this);
-        this.handleNeutral = this.handleNeutral.bind(this);
-        this.handleBad = this.handleBad.bind(this);
-        this.countTotalFeedback = this.countTotalFeedback.bind(this);
-        this.countPositiveFeedbackPercentage = this.countPositiveFeedbackPercentage.bind(this);
-    }
-    handleGood = () =>
-    {
-        for (let i = 0; i < 1; i += 1)
-        {
-            this.setState({ good: this.state.good + 1 });
-        }
-        this.countTotalFeedback();
-        this.countPositiveFeedbackPercentage();
-    };
-    handleNeutral = () =>
-    {
-        for (let i = 0; i < 1; i += 1)
-        {
-            this.setState({ neutral: this.state.neutral + 1 });
-        }
-        this.countTotalFeedback();
-        this.countPositiveFeedbackPercentage();
-    };
-    handleBad = () =>
-    {
-        for (let i = 0; i < 1; i += 1)
-        {
-            this.setState({ bad: this.state.bad + 1 });
-        }
-        this.countTotalFeedback();
-        this.countPositiveFeedbackPercentage();
-    };
-    countTotalFeedback = () =>
-    {
-        this.setState(totalState =>
-        {
-            return { total: totalState.good + totalState.neutral + totalState.bad };
-        });
-    };
-    countPositiveFeedbackPercentage = () =>
-    {
-        this.setState(percentageState =>
-        {
-            return { positivePercentage: 100 * percentageState.good / (percentageState.good === 0 && percentageState.neutral === 0 && percentageState.bad === 0 ? 100 : percentageState.good + percentageState.neutral + percentageState.bad) };
-        });
-    };
-    render()
-    {
-        const { good, neutral, bad, total, positivePercentage, text } = this.state;
-
-        return (
-            <div>
-                <div className={Css.block}>
-                    <button className={Css['button']} type="button" onClick={this.handleGood}>Good</button>
-                    <button className={Css['button']} type="button" onClick={this.handleNeutral}>Neutral</button>
-                    <button className={Css['button']} type="button" onClick={this.handleBad}>Bad</button>
-                </div>
-                <div>
-                    {good > 0 || neutral > 0 || bad > 0 || total > 0 || positivePercentage > 0 ?
-                        (<div className={Css.block_text}>
-                            <h2>Statistics</h2>
-                            <div className={Css.block_span}>
-                                <label className={Css.label}>Good:</label>
-                                <span className={Css['span']}>{good};</span>
-                            </div>
-                            <div className={Css.block_span}>
-                                <label className={Css.label}>Neutral:</label>
-                                <span className={Css['span']}>{neutral};</span>
-                            </div>
-                            <div className={Css.block_span}>
-                                <label className={Css.label}>Bad:</label>
-                                <span className={Css['span']}>{bad};</span>
-                            </div>
-                            <div className={Css.block_span}>
-                                <label className={Css.label}>Total:</label>
-                                <span className={Css['span']}>{total};</span>
-                            </div>
-                            <div className={Css.block_span}>
-                                <label className={Css.label}>Positive feedback:</label>
-                                <span className={Css['span']}>{positivePercentage}%;</span>
-                            </div>
-                        </div>) : (<Notification message="" />
-                    )}
-                </div>
-            </div>
-        );
-    }
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+export const Statistics = ({ title, stats }) =>
+{
+    return (
+        <section className={Css.statistics}>
+            {title && <h2 className={Css.title}>{title}</h2>}
+            <ul className={Css.stat_list}>
+                {stats.map((statistic) =>
+                {
+                    return (
+                        <li className={Css.item} key={statistic.id} style={{backgroundColor: getRandomHexColor()}}>
+                            <span className={Css['label']}>{statistic.label}</span>
+                            <span className={Css['percentage']}>{statistic.percentage}%</span>
+                        </li>
+                    );
+                })}
+            </ul>
+        </section>
+    );
+};
+/*
 Statistics.propTypes =
 {
-    good: PropTypes.number.isRequired,
-    neutral: PropTypes.number.isRequired,
-    bad: PropTypes.number.isRequired,
-    total: PropTypes.number.isRequired,
-    positivePercentage: PropTypes.number.isRequired
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired,
+            percentage: PropTypes.number.isRequired
+        })
+    ).isRequired
 };
+*/
